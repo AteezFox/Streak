4f282e319248865cb64ddd1dd029b1b23572ce20
unknown <attila.gyurcsak@gmail.com>
2025-01-09 21:20:24 +0100

    adatbaszas

---
+++ streakbackendtest/src/main/java/com/example/streakbackendtest/deliver/Deliver.java
@@ -0,0 +1,93 @@
+package com.example.streakbackendtest.deliver;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table
+public class Deliver {
+
+    @Id
+    @SequenceGenerator(
+            name = "deliver_sequence",
+            sequenceName = "deliver_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "deliver_sequence"
+    )
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String phone;
+
+    public Deliver() {
+    }
+
+    public Deliver(Long id, String name, String email, String password, String phone) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+    }
+
+    public Deliver(String name, String email, String password, String phone) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Deliver{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+}
