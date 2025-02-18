c52bebd9f2b6714e04d53bf8d7075346c29d8f01
AteezFox <attila.gyurcsak@gmail.com>
2025-02-18 18:25:17 +0100

    fasz

--- streak/src/assets/ComponentsCeo/CreateCeo/CreateCeo.tsx
+++
@@ -1,7 +0,0 @@
-export default function CreateCeo() {
-    return (
-        <div>
-            <h2>Create Ceo</h2>
-        </div>
-    );
-}
--- streak/src/assets/ComponentsCeo/UpdateCeo/UpdateCeo.tsx
+++
@@ -1,7 +0,0 @@
-export default function UpdateCeo() {
-    return (
-        <div>
-            <h2>Update Ceo</h2>
-        </div>
-    );
-}
