2b31029bf45ab57a6c4a6c55fbce4bdacdf3bdcc
AteezFox <attila.gyurcsak@gmail.com>
2025-03-31 10:06:25 +0200

    navbar átdolgozás cím typeshit egyebek

--- streak/src/App.jsx
+++ streak/src/App.jsx
@@ -1,15 +1,20 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import UserProfile from "./assets/Role/User/UserComponents/UserProfile/UserProfile.jsx";
 import Body from "./assets/Components/PageBody/Body.jsx"
 import UserInterface from "./assets/Role/User/UserComponents/UserInterface/UserInterface.jsx";
+import ErrorPage from "./assets/Role/User/UserComponents/ErrorPage/ErrorPage.jsx";
+import Orders from "./assets/Role/User/UserComponents/Orders/Order.jsx";
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/yourhome" index={true} element={<UserInterface/>} />
+                <Route path="/" element={<Body />} index={true} />
+                <Route path="/yourhome" element={<UserInterface/>} />
                 <Route path="/profile" element={<UserProfile />} />
+                <Route path="/orders" element={<Orders />} />
+                <Route path="*" ErrorBoundary={true} element={<ErrorPage />} />
             </Routes>
         </Router>
     );
