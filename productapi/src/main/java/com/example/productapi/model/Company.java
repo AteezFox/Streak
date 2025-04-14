fe704a459cad1406a9d5cd144f2fbb3af6e91f5a
SoapMacJeez <85246371+SoapMacJeez@users.noreply.github.com>
2025-04-14 17:16:12 +0200

    Some stuff
    skibidi sigma

---
+++ productapi/src/main/java/com/example/productapi/model/Company.java
@@ -0,0 +1,14 @@
+package com.example.productapi.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
