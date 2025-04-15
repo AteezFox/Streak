e7d8e41b202982ec977254773d250223e2ce1af4
SoapMacJeez <85246371+SoapMacJeez@users.noreply.github.com>
2025-04-15 17:14:33 +0200

    asdasdasa
    asdasd

--- productapi/src/main/java/com/example/productapi/config/SecurityConfig.java
+++ productapi/src/main/java/com/example/productapi/config/SecurityConfig.java
@@ -15,7 +15,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> {})
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/auth/**",
