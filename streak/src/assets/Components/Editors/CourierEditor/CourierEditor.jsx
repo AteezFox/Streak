b906c99eee3fa401c0a1c8273fb61bcda9d55ac4
AteezFox <attila.gyurcsak@gmail.com>
2025-04-25 18:51:55 +0200

    editor oldalra való átvezetés

---
+++ streak/src/assets/Components/Editors/CourierEditor/CourierEditor.jsx
@@ -0,0 +1,7 @@
+export default function CeoEditor() {
+    return (
+        <>
+            
+        </>
+    );
+}
---
+++ streak/src/assets/Components/Editors/UserEditor/UserEditor.jsx
@@ -0,0 +1,8 @@
+export default function UserEditor() {
+    return (
+        <>
+            <h1>User Editor</h1>
+            <p>This is the User Editor component.</p>
+        </>
+    );
+}
